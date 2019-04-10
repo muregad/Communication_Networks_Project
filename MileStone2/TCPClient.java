91c52f635fdf232722df428b88b0b2acc34d02b2
muregad <47953868+muregad@users.noreply.github.com>
2019-04-11 01:23:24 +0200

    Add files via upload

--- MileStone2/TCPClient.java
+++ MileStone2/TCPClient.java
@@ -1,86 +1,52 @@
-package MileStone1;
+package MileStone2;
 
-import java.net.*; 
-import java.io.*; 
-  
-public class TCPClient { 
-    // initialize socket and input output streams 
-    private Socket socket            = null; 
-    private DataInputStream  input   = null; 
-    private DataInputStream  inputٍServer   = null;
-    private DataOutputStream out     = null; 
-  
-    // constructor to put ip address and port 
-    public TCPClient(String address, int port) 
-    { 
-        // establish a connection 
-        try
-        { 
-        	
-        	 // takes input from terminal 
-            input  = new DataInputStream(System.in); 
-            
-            while(!input.readLine().equals("Connect"));
-            
-            socket = new Socket(address, port); 
-            System.out.println("Connected"); 
-  
-            // sends output to the socket 
-            out = new DataOutputStream(socket.getOutputStream()); 
-            inputٍServer = new DataInputStream(socket.getInputStream());
-        } 
-        catch(UnknownHostException u) 
-        { 
-            System.out.println(u); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-  
-        // string to read message from input 
-        String line1 = "" , line2 = ""; 
-  
-        // keep reading until "Over" is input 
-        while (!(line1.equals("Over") || line2.equals("Over"))) 
-        { 
-            try
-            { 
-                line1 = input.readLine(); 
-                out.writeUTF(line1); 
-            } 
-            catch(IOException i) 
-            { 
-                System.out.println(i); 
-            } 
-            
-            try
-            { 
-                line2 = inputٍServer.readUTF(); 
-                System.out.println(line2); 
-            } 
-            catch(IOException i) 
-            { 
-                System.out.println(i); 
-            } 
-        } 
-  
-        // close the connection 
-        try
-        { 
-            input.close(); 
-            inputٍServer.close();
-            out.close(); 
-            socket.close(); 
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        TCPClient client = new TCPClient("10.0.1.184", 5000); 
-    } 
-} 
+import java.net.*;
+import java.io.*;
+
+public class TCPClient {
+	DataOutputStream dos;
+	BufferedReader inServer;
+	Socket socket;
+	BufferedReader inConsole;
+	
+public TCPClient(String ip,int port) {
+	try {
+		inConsole = new BufferedReader(new InputStreamReader(System.in));
+		
+		while (!inConsole.readLine().equals("Connect"));
+	try {
+		socket= new Socket(ip, port);
+		dos = new DataOutputStream(socket.getOutputStream());
+		inServer =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		String sentence ="";
+		String serverSentence="";
+		while (!(sentence.equals("over")||serverSentence.equals("over"))) {
+			if (inConsole.ready()) {
+				sentence=inConsole.readLine();
+				dos.writeBytes(sentence +"\n");
+			}
+			if (inServer.ready()) {
+				serverSentence=inServer.readLine();
+				System.out.println(serverSentence);
+			}
+				
+		}
+	}
+	catch(Exception e) {
+		System.out.println(e.getMessage());
+	}
+	finally {
+		socket.close();
+	}
+	}
+	catch (IOException x) {
+		System.out.println(x.getMessage());
+	}
+
+	
+}
+
+public static void main (String[]args) {
+	TCPClient client=new TCPClient("localhost", 5000);
+}
+}
--- MileStone2/TCPServer.java
+++ MileStone2/TCPServer.java
@@ -1,77 +1,61 @@
-package MileStone1;
+package MileStone2;
 
-import java.io.*;
-import java.net.*; 
-  
-public class TCPServer { 
-    
-    private Socket          socket   = null; 
-    private ServerSocket    server   = null; 
-    private DataInputStream in       =  null; 
-    private DataInputStream inConsole       =  null; 
-    private DataOutputStream out     = null; 
-  
-     
-    public TCPServer(int port) 
-    { 
-        
-        try
-        { 
-            server = new ServerSocket(port); 
-            System.out.println("Server started"); 
-  
-            System.out.println("Waiting for a client ..."); 
-  
-            socket = server.accept(); 
-            System.out.println("Client accepted"); 
-  
-            
-            in = new DataInputStream(new BufferedInputStream(socket.getInputStream())); 
-            inConsole = new DataInputStream(new BufferedInputStream(System.in));
-            out = new DataOutputStream(socket.getOutputStream()); 
-            
-            String line1 = "" , line2 = ""; 
-  
-            
-            while (!(line1.equals("Over") || line2.equals("over"))) 
-            { 
-                try
-                { 
-                    line1 = in.readUTF(); 
-                    System.out.println(line1); 
-  
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-                
-                try
-                { 
-                    line2 = inConsole.readLine(); 
-                    out.writeUTF(line2);
-                } 
-                catch(IOException i) 
-                { 
-                    System.out.println(i); 
-                } 
-            } 
-            System.out.println("Closing connection"); 
-  
-            
-            socket.close(); 
-            in.close(); 
-            out.close(); 
-            inConsole.close();
-        } 
-        catch(IOException i) 
-        { 
-            System.out.println(i); 
-        } 
-    } 
-  
-    public static void main(String args[]) 
-    { 
-        TCPServer server = new TCPServer(5000); 
-    } 
-} 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	
+	DataOutputStream doc;
+	BufferedReader inClient;
+	Socket socket;
+	ServerSocket welcomeSocket;
+	BufferedReader inConsole;
+	public TCPServer(int port) throws IOException {
+		try {
+			welcomeSocket =new ServerSocket(port);
+			System.out.println("Initilized....."+"\n"+"Waiting for a client");
+			socket= welcomeSocket.accept();
+			System.out.println("Connected");
+			doc =new DataOutputStream(socket.getOutputStream());
+			inClient=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			inConsole= new BufferedReader(new InputStreamReader(System.in));
+			
+			String sentence="";
+			String clientSentence="";
+			while (!(sentence.equals("over")||clientSentence.equals("over"))) {
+				if (inConsole.ready()) {
+					sentence=inConsole.readLine();
+					doc.writeBytes(sentence +"\n");
+				}
+				if (inClient.ready()) {
+					clientSentence=inClient.readLine();
+					System.out.println(clientSentence);
+				}
+					
+			}
+		}
+		catch(IOException e1) {
+			System.out.println(e1.getMessage());
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			welcomeSocket.close();
+			socket.close();
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException{
+		
+		TCPServer server=new TCPServer(5000);
+		
+
+	}
+
+}
